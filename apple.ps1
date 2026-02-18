2e5ee9266c37fec45785dfde4b5cd0e40c397f50
Murali1239 <praneethbonam14319@gmail.com>
2026-02-18 05:52:04 +0530

    newuser

---
+++ apple.ps1
@@ -0,0 +1,20 @@
+$name = "appu"
+$age = 10
+if ($age -ge 18){
+write-output "my name is $name and my age is $age"
+}
+else{
+    write-output "not eligible"
+}
+for ($i=1; $i -le 5; $i++){
+ write-output $i
+ }
+$names="java","python"
+foreach($name in $names){
+    write-output $name
+}
+$i = 1
+while($i -le 5){
+    write-output $i
+    $i++
+}
